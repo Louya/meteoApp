6c2841e5151cd2e344fb590c63cf7ad6a096e510
Louya <fabien.c@codeur.online>
2019-01-17 15:39:39 +0000

    [JS][CSS][HTML] Ajout du graph

--- Assets/js/chart.js
+++ Assets/js/chart.js
@@ -1,26 +1,52 @@
 new Chart(document.getElementById("summaryChart"), {
     type: 'line',
     data: {
-      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+      labels: ['00:00', '06:00', '12:00', '18:00', '24:00'],
       datasets: [{ 
-          data: [86,114,106,106,107,111,133,221,783,2478],
-          label: "Africa",
-          borderColor: "#3e95cd",
-          fill: false
+          data: [1, 3, 8, 5, 2],
+          yAxisID: 'temperature',
+          label: "Température",
+          borderColor: "#980000",
+          fill: true
         }, { 
-          data: [282,350,411,502,635,809,947,1402,3700,5267],
-          label: "Asia",
-          borderColor: "#8e5ea2",
-          fill: false
+          data: [0, 2, 5, 10, 3],
+          yAxisID: 'rain',
+          label: "Précipitation",
+          borderColor: "#030340",
+          fill: true
         }
       ]
     },
     options: {
-      fill: true,
-      title: {
-        display: true,
-        text: 'World population per region (in millions)'
-      }
+        legend:{
+            display: false
+        },
+        title: {
+            display: false,
+            text: 'World population per region (in millions)'
+        },
+        scales: {
+            yAxes: [{
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: 'left',
+                id: 'temperature',
+            }, {
+                type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                display: true,
+                position: 'right',
+                id: 'rain',
+                ticks: {
+                    min: 0,
+                    max: 100
+                },
+
+                // grid line settings
+                gridLines: {
+                    drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
+            }],
+        }
     }
   });
   
