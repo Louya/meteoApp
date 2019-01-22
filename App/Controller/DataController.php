27bcef662a8dd1293cb5605bf63313800ea41c34
abdelkrimn <abdelkrim.n@codeur.online>
2019-01-22 08:49:24 +0000

    fusion master

--- App/Controller/DataController.php
+++ App/Controller/DataController.php
@@ -10,19 +10,16 @@ use App\Models\Users;
 class DataController extends Controller{
 
     /** Route /weather */
-
     public function weather(){
         return $this->twig->render('weather.html.twig');
     }
 
     /** Route /weather/get */
-
-
     public function get(){
-
+      
         function callAPI($method, $url, $data){
             $curl = curl_init();
- 
+    
             switch ($method){
             case "POST":
                 curl_setopt($curl, CURLOPT_POST, 1);
@@ -38,7 +35,7 @@ class DataController extends Controller{
                 if ($data)
                     $url = sprintf("%s?%s", $url, http_build_query($data));
             }
- 
+    
             // OPTIONS:
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_HTTPHEADER, array(
@@ -65,19 +62,104 @@ class DataController extends Controller{
 
         $get_data = callAPI('GET', $link, false);
         echo json_encode($get_data);
-        // $response = json_decode($get_data, true);
-        // $errors = $response['response']['errors'];
-        // $data = $response['response']['data'][0];
-
-
-        // echo json_encode($response);
-
     }
 
-    /** Route /404 */
+        
+    // /** Route /weather/city */
+    // public function city_location(){
 
+    //     // if(isset($_POST["latitude"]) && isset($_POST["longitude"])){
+            
+    //         $latitude = $_POST["latitude"];
+    //         $longitude = $_POST["longitude"];
+    //         echo $latitude;
+
+    //         // $latitude = 67.3;
+    //         // $longitude = 6.17;
+    //         // function callAPI($method, $url, $data){
+    //         //     $curl = curl_init();
+        
+    //         //     switch ($method){
+    //         //     case "POST":
+    //         //         curl_setopt($curl, CURLOPT_POST, 1);
+    //         //         if ($data)
+    //         //             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    //         //         break;
+    //         //     case "PUT":
+    //         //         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    //         //         if ($data)
+    //         //             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+    //         //         break;
+    //         //     default:
+    //         //         if ($data)
+    //         //             $url = sprintf("%s?%s", $url, http_build_query($data));
+    //         //     }
+        
+    //         //     // OPTIONS:
+    //         //     curl_setopt($curl, CURLOPT_URL, $url);
+    //         //     curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    //         //     'APIKEY: 111111111111111111111',
+    //         //     'Content-Type: application/json',
+    //         //     ));
+    //         //     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //         //     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            
+    //         //     // EXECUTE:
+    //         //     $result = curl_exec($curl);
+    //         //     if(!$result){die("Connection Failure");}
+    //         //     curl_close($curl);
+    //         //     return $result;
+    //         // }
+                
+    //         // $ms = round(microtime(true));        
+    //         // $link = "https://api.darksky.net/forecast/dc13d8d3db140a701e2aac4edfdfcfb3/".$latitude.",".$longitude.",".$ms."?lang=fr&units=auto";
+        
+    //         // $get_data = callAPI('GET', $link, false);
+    //         // echo json_encode($get_data);
+    //         // // echo json_decode($p);
+    //         // $s = $p[5];
+    //         //  $s;
+    //     // }
+    // }
+    
+    
+    /** Route /404 */
     public function notFound(){
         return $this->twig->render('notFound.html.twig');
     }
 
+    function callAPI($method, $url, $data){
+        $curl = curl_init();
+
+        switch ($method){
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // OPTIONS:
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'APIKEY: 111111111111111111111',
+        'Content-Type: application/json',
+        ));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    
+        // EXECUTE:
+        $result = curl_exec($curl);
+        if(!$result){die("Connection Failure");}
+        curl_close($curl);
+        return $result;
+    }
 }
