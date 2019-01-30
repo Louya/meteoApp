10f619d6604fc9906260bec5fbd4eafb44288f55
Isochore <jules.p@codeur.online>
2019-01-30 14:50:42 +0000

    merge

--- meteo.sql
+++ meteo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Mar 29 Janvier 2019 à 08:36
+-- Généré le :  Mer 30 Janvier 2019 à 15:39
 -- Version du serveur :  10.3.12-MariaDB-1:10.3.12+maria~bionic-log
 -- Version de PHP :  7.2.10-0ubuntu0.18.04.1
 
@@ -42,35 +42,22 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `prenom`, `sexe`, `color`, `mail`, `pass`, `adresse`, `ville`) VALUES
-(1, 'qdsdsqd', 'homme', '#FFD699', 'jqdqdjslq@sodifhs.dqsd', 'MYMdx7kWHgjGm151PLbOnQ==', 'ici', NULL),
-(2, 'Jean', 'femme', '#553301', 'qsopfjs@mail.com', 'EH8JsQbpneAyUPpON9cS3A==', '7 rue des casernes', 'Vesoul'),
-(3, 'tr', 'homme', '#FFC266', 'ertret@mail.fr', 'txwPNy6KB7dxmGwOGBVUjw==', 'dgffgg', 'gdgfdg'),
-(4, 'sdqdqsdqs', 'homme', '#FFC266', 'sffdsf@sdfsd.fsqd', 'iBFdCr4Yn42Nf1LeHReFOw==', 'sdsdkldn', 'sqdqsd'),
-(5, 'zasdfsdf', 'homme', '#FFD699', 'zgdfgdfg@esfsfd.sdfsf', 'UEmG0bnVgl4mdP3uS6VVsQ==', 'dfsdfsfd', 'sdfsdf'),
-(6, 'David', 'homme', '#FFC266', 'qsdoih@sdfsd.dsf', 'eyqio6Xn2zl2BTpkQ08wEA==', 'avenue renÃ©', 'nantes'),
-(7, 'David', 'homme', '#FFC266', 'qsdoih@sdfsd.dsfg', 'eyqio6Xn2zl2BTpkQ08wEA==', '7 RUE DES CASERNES', 'Vesoul'),
-(8, 'Pedro', 'homme', '#FFC266', 'dfdfdg@sdfd.fdf', 'ZcbyTR8E+EcExryhJbed8g==', 'Rue du Bois', 'Ã‰chenoz-le-Sec'),
-(9, 'HervÃ©', 'homme', '#FFC266', 'dfdfdg@sdfd.fdfriu', 'ZcbyTR8E+EcExryhJbed8g==', 'Rue du Bois', 'dspohdoigh'),
-(10, 'Germaine', 'femme', '#FFEFD6', 'dsffd@dsf.dffd', 'T2ay5kWY2dXsdV6hSvF9jw==', 'sdfhf', 'sfoisfoi'),
-(11, 'Manon', 'homme', '#FFD699', 'sfd@dsff.dfs', 'Mz3G2SZ04WcU/aSldTDSVg==', 'repoiejroie', 'sdffsh'),
-(12, 'Thomas', 'femme', '#FFD699', 'sdfihsdiu@df.df', 'Msx7cOJOz35x6OTXpS28Zw==', 'sdfjfoi', 'sdfpofjdsoi'),
-(13, 'Aragorn', 'homme', '#FFD699', 'dgfg.dgf@dfgdg.dg', '9zg+vSLF5iVWyUu7WdrgOA==', 'sdhjjo', 'sdfidj'),
-(14, 'AurÃ©lien', 'homme', '#FFD699', 'sdgdgdg.dsf@dsf.fds', 'wtrzlpey7tcNL0DBSbMq6g==', '7 rue des casernes', 'Vesoul'),
-(15, 'KÃ©vin', 'homme', '#FFD699', 'dgg@sdf.df', '+Re+0JArT9hCt/08r7aocA==', '7 rue des casernes', 'Vesoul'),
-(16, 'Claude', 'homme', '#FFD699', 'sfd.dff@dfd.df', 'd1XB47zMo6j8ig1krh6VHQ==', '7 rue des casernes', 'Vesoul'),
-(17, 'Natasha', 'homme', '#FFEFD6', 'sdf@sdf.fd', 'd/X9vYQfq9KJE5u2BFg8Ng==', '7 rue des casernes', 'Vesoul'),
-(18, 'Dimitri', 'homme', '#FFD699', 'dgdgg.fdggd@dfdf.d', 'BRbY0C031/q4slXki31TTw==', '7 rue des casernes', 'Vesoul'),
-(19, 'Claire', 'femme', '#FFD699', 'sefrdg@sdfdf.fdf', '9OXBKVP6HfZ84+U60NyaLA==', '7 rue des casernes', 'Vesoul'),
-(20, 'Claire', 'femme', '#FFD699', 'sefrdg@sdfdf.fdfr', '9OXBKVP6HfZ84+U60NyaLA==', '7 rue des casernes', 'Vesoul'),
-(21, 'Max', 'femme', '#FFD699', 'iuhdruig@diufhdfi.ds', 'ateQPrrBzY8GAYEGfUGF1w==', '7 rue des casernes', 'Vesoul'),
-(22, 'Rakesh', 'homme', '#FFD699', 'oidhwdio@sdggd.d', 'B/nNNBLndDPymcT6Xzf3tg==', 'Rue du Bois', 'Ã‰chenoz-le-Sec'),
-(23, 'Valentin', 'homme', '#FFEFD6', 'dsffdsf@dff.f', 'YMaMWZjsntNO1e0SDkGT/A==', '7 rue des casernes', 'Vesoul'),
-(24, 'Alexandre', 'homme', '#FFD699', 'sdgd@dfggf.dff', 'VrE3FuDzMZ2uqdgIR6MKGg==', '7 rue des casernes', 'Vesoul'),
-(25, 'Roger', 'homme', '#FFD699', 'dsgf@dfs.fdss', 'm9L3R9oeEt8DYC20dkzrgQ==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul'),
-(26, 'Lucien', 'homme', '#FFD699', 'mail@mail.mail', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul'),
-(27, 'Lucie', 'femme', '#FFD699', 'mail1@mail.mail', 'JJ+WFIV51ny32cHsd2hxRA==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul'),
-(28, 'Antoine', 'homme', '#FFD699', 'test@test.test', 'JhOZ0WnNv6L51JRFJRxgpw==', 'Rue de Praley', 'Vesoul'),
-(29, 'Abdelkrim', 'homme', '#553301', 'lemail@lemail.lemail', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul');
+(1, 'Jules', NULL, NULL, 'mail@mail.mail', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul'),
+(2, 'Fabien', NULL, NULL, 'unmail@unmail.unmail', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul'),
+(3, 'Abdel', NULL, NULL, 'deuxmail@deuxmail.deuxmail', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue du Docteur Jean-Georges Girard', 'Vesoul'),
+(4, 'Antoine', NULL, NULL, 'sefdrtedte@sdfdsf.ssdf', '4oXBfVmjwKV2X7r7/MZCxA==', 'Rue du Docteur Jean-Georges Girard', 'dsffsdfdVesoul'),
+(5, 'Antoine', NULL, NULL, 'sefdrtedte@sdfdsf.ssdfgf', '4oXBfVmjwKV2X7r7/MZCxA==', 'Rue du Docteur Jean-Georges Girard', 'gfdighdf'),
+(6, 'Pierre', NULL, NULL, 'laim@laim.laim', '0XxbVTAzmD4JQqWnZY8mGg==', 'Baniyas', 'Dubai'),
+(7, 'Damien', NULL, NULL, 'test@mail.fr', '0XxbVTAzmD4JQqWnZY8mGg==', 'Chemin des Perches', 'Pusey'),
+(8, 'AurÃ©lien', NULL, NULL, 'le@le.le', '0XxbVTAzmD4JQqWnZY8mGg==', 'Chemin des Perches', 'Pusey'),
+(9, 'Test', NULL, NULL, 'el@el.el', '0XxbVTAzmD4JQqWnZY8mGg==', 'Chemin des Perches', 'Pusey'),
+(10, 'dfssdffs', NULL, NULL, 'qsdqsd@dfdf.dsf', 'e8XyvkZhGrgUMFuwzcr4gA==', 'Chemin des Perches', 'Pusey'),
+(11, 'Jean', NULL, NULL, 'mail1@mail1.mail1', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue de Vesoul', 'BesanÃ§on'),
+(12, 'Antoine', NULL, NULL, 'mail2@mail2.mail2', '0XxbVTAzmD4JQqWnZY8mGg==', 'Rue de Vesoul', 'BesanÃ§on'),
+(13, 'sdfiojdoifhdf', NULL, NULL, 'sdghdfio@dfghd.dfgf', 'eUtJWkCLmuzpQjP1puBtiw==', 'Chemin des Perches', 'Pusey'),
+(14, 'sdfiojdoifhdf', NULL, NULL, 'sdghdfio@dfghd.dfgfe', 'eUtJWkCLmuzpQjP1puBtiw==', 'Chemin des Perches', 'Pusey'),
+(15, 'sdfdsfdsfqff', NULL, NULL, 'sdfiudgui@diufg.sdf', 'hlVZB71+DMZyPiAfcWHX2Q==', 'Chemin des Perches', 'Pusey'),
+(16, 'sdfdsfdsfqff', NULL, NULL, 'sdfiudgui@diufg.sdffdr', 'hlVZB71+DMZyPiAfcWHX2Q==', 'Chemin des Perches', 'Pusey');
 
 -- --------------------------------------------------------
 
@@ -125,7 +112,7 @@ ALTER TABLE `weather`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT pour la table `weather`
 --
